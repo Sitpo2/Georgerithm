3115826807b1edccfb8e8f23602f856f6959129a
nasa985 <nasa985@naver.com>
2025-08-06 01:04:30 +0900

    snail

--- 유재신/day02_0805/Solution/src/SEWA_1954_snail/practice.java
+++ 유재신/day02_0805/Solution/src/SEWA_1954_snail/practice.java
@@ -1,38 +1,57 @@
 
 package SEWA_1954_snail;
 
+import java.util.Scanner;
+
 public class practice {
-	static int[] dr = { 0, 1, 0, -1}; // right down left up
-	static int[] dc = { 1, 0, -1, 0}; // right down left up
+	
+	//변화값 주가 위해
+	static int[] dr = {0, 1, 0, -1}; //행변화 오른쪽, 아래, 왼쪽, 위
+	static int[] dc = {1, 0, -1, 0}; //열변화
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 		
+		//테스트케이스 입력
 		int T = sc.nextInt();
-		
-		int nowR = 0; //초기값 설정(0,0)
-		int nowC = 0;
-		int dir = 0; // 방향 인덱스 설정(오른쪽 먼저 시작)
-		
-		for (int n =1; n<=T*T; n++) {
+		for(int t = 1; t<=T; t++) {
 			
+			//배열에 있는 수 정의
+			int nowr = 0;
+			int nowc = 0;
+			int dir = 0; //방향 정할 때 씀
 			
+			//배열 입력
+			int [][] arr = new int [t][t];
 			
-			
-		
-			for (int i = 0; i < 4; i++) {
-				// i = 0
-				nextR = nowR + dr[i];
-				nextC = nowC + dc[i];
+			//한칸씩 이동할 때마다 수 증가함
+			for(int n=1; n<=t*t; n++) {
+				arr[nowr][nowc] = n;
 				
-				if (nextR >= 0 && nextR < N) {
-					nowR = nextR;
-					
-					arr[nowR][nowC] = n;
-					
+				//다음에 나올 행, 열 값 구하기(
+				int nextr = nowr + dr[dir];
+				int nextc = nowc + dc[dir];
+				
+				if(nextr<0 || nextc<0 || nextr>=t || nextc>=t || arr[nextr][nextc]!=0) {
+					dir = ((dir+1) % 4);
 				}
+				nextr = nowr + dr[dir];
+				nextc = nowc + dc[dir];
+				
+				//값 초기화 시켜야 함
+				nowr = nextr;
+				nowc = nextc;				
 			}
+			
+			//완성값 출력
+			System.out.println("#" + t);
+			for(int r=0; r<t; r++) {
+				for(int c=0; c<t; c++) {
+					System.out.print(arr[r][c] + " ");
+				}
+				System.out.println("");
+			}
+
 		}
 	}
-
 }
