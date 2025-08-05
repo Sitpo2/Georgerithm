ab386ffd839f590e1e8118ffb9ab9e811734c968
nasa985 <nasa985@naver.com>
2025-08-05 22:56:42 +0900

    snail

--- 유재신/day02_0805/Solution/src/SEWA_1954_snail/Solution.java
+++ 유재신/day02_0805/Solution/src/SEWA_1954_snail/Solution.java
@@ -7,38 +7,51 @@ import java.util.Scanner;
 public class Solution {
 	
 	//변화값 주기
-	static int [] dr = {0, 1, 0, -1}; //right, down, left, up
-	static int [] dc = {1, 0, -1, 0}; //right, down, left, up
-	
+	static int [] dr = {0, 1, 0, -1}; //오른쪽, 아래, 왼쪽, 위
+	static int [] dc = {1, 0, -1, 0}; //오른쪽, 아래, 왼쪽, 위
+			
 	public static void main(String[] args) throws FileNotFoundException {
 		File file = new File("./src/SEWA_1954_snail/snail.txt");
 		Scanner sc = new Scanner(file);
-		
+	
 		int T = sc.nextInt();
-		
+		for(int tc = 1; tc<=T; tc++) {
+
 		//현재 위치, 미래 위치 기록
-		int nowR = 0; //초기값 설정(0,0)
+		int nowR = 0; // 초기값 arr[0][0] = 0 설정
 		int nowC = 0;
-		int dir = 0; //방향 설정
+		int dir = 0; //방향 정하기
 		
 		//배열 생성
-		int [][] arr = new int[T][T];
-		int N = arr.length;
+		int[][] arr = new int[tc][tc]; //모든 값은 0으로 정해짐.
 		
-		//각 칸마다 1씩 증가하도록 설정(1부터)
-		for(int n = 1; n<T*T; n++) {
+		//각 칸마다 1씩 증가하도록 설정(1부터 T*T까지 증가)
+		for(int n = 1; n<=tc*tc; n++) {
 			arr[nowR][nowC] = n;
 			
 			int nextR = nowR + dr[dir];
 			int nextC = nowC + dc[dir];
-		}
-			nowR = nextR;
 			
-		
+			//방향 전환하기
+			if(nextR<0 || nextR>=tc || nextC<0 || nextC>=tc || arr[nextR][nextC] !=0) {
+				dir = (dir+1) % 4;
+				nextR = nowR+dr[dir];
+				nextC = nowC+dc[dir];
 			}
-
+			nowR = nextR;
+			nowC = nextC;
 		}
+		//완성된 값 출력하기
+		System.out.println("#" + tc);
+		for(int r = 0; r<tc; r++) {
+			for(int c = 0; c<tc; c++) {
+				 System.out.print(arr[r][c] + " ");
+			}
+			 System.out.println("");
+		} 
+		
 		
 	}
 
 	}
+}
--- 유재신/day02_0805/Solution/src/SEWA_1954_snail/practice2.java
+++ 유재신/day02_0805/Solution/src/SEWA_1954_snail/practice2.java
@@ -1,46 +1,54 @@
 package SEWA_1954_snail;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Solution {
-    static int[] dr = {0, 1, 0, -1}; // right, down, left, up
-    static int[] dc = {1, 0, -1, 0};
+public class practice2 {
+	
+	//변화값 주기
+	static int [] dr = {0, 1, 0, -1}; //오른쪽, 아래, 왼쪽, 위
+	static int [] dc = {1, 0, -1, 0}; //오른쪽, 아래, 왼쪽, 위
+			
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		for(int tc = 1; tc<=10; tc++) {
+			int T = sc.nextInt();
+		//현재 위치, 미래 위치 기록
+		int nowR = 0; // 초기값 arr[0][0] = 0 설정
+		int nowC = 0;
+		int dir = 0; //방향 정하기
+		
+		//배열 생성
+		int[][] arr = new int[T][T]; //모든 값은 0으로 정해짐.
+		
+		//각 칸마다 1씩 증가하도록 설정(1부터 T*T까지 증가)
+		for(int n = 1; n<=T*T; n++) {
+			arr[nowR][nowC] = n;
+			
+			int nextR = nowR + dr[dir];
+			int nextC = nowC + dc[dir];
+			
+			//방향 전환하기
+			if(nextR<0 || nextR>=T || nextC<0 || nextC>=T || arr[nextR][nextC] !=0) {
+				dir = (dir+1) % 4;
+				nextR = nowR+dr[dir];
+				nextC = nowC+dc[dir];
+			}
+			nowR = nextR;
+			nowC = nextC;
+		}
+		//완성된 값 출력하기
+		System.out.println("#" + tc);
+		for(int r = 0; r<T; r++) {
+			for(int c = 0; c<T; c++) {
+				 System.out.print(arr[r][c] + " ");
+			}
+			 System.out.println("");
+		} 
+		
+		
+	}
 
-    public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("./src/SEWA_1954_snail/snail.txt");
-        Scanner sc = new Scanner(file);
-
-        int T = sc.nextInt();
-
-        int[][] arr = new int[T][T];
-
-        int nowR = 0, nowC = 0;  // 현재 위치
-        int dir = 0;             // 방향 인덱스
-        for (int num = 1; num <= T * T; num++) {
-            arr[nowR][nowC] = num;
-
-            int nextR = nowR + dr[dir];
-            int nextC = nowC + dc[dir];
-
-            // 범위 벗어나거나 이미 채워진 칸이면 방향 전환
-            if (nextR < 0 || nextR >= T || nextC < 0 || nextC >= T || arr[nextR][nextC] != 0) {
-                dir = (dir + 1) % 4;
-                nextR = nowR + dr[dir];
-                nextC = nowC + dc[dir];
-            }
-
-            nowR = nextR;
-            nowC = nextC;
-        }
-
-        // 출력
-        for (int i = 0; i < T; i++) {
-            for (int j = 0; j < T; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
+	}
 }
